20f150e6e67a5f6c7569b9ea0e3a3eab55b73d97
Hiten Pankhania <pankhaniahiten4@gmail.com>
2023-09-01 16:49:30 +0530

    okiee

--- script.js
+++ script.js
@@ -1,4 +1,35 @@
-const scroll = new LocomotiveScroll({
-    el: document.querySelector('#main'),
-    smooth: true
-});
+// const scroll = new LocomotiveScroll({
+//     el: document.querySelector('#main'),
+//     smooth: true
+// });
+
+function firstPageAnim(){
+    var tl=gsap.timeline();
+
+
+    tl.from("#nav" ,{
+        y: '-10',
+        opacity:0,
+        duration:2,
+        ease:Expo.easeInOut
+    })
+    .to(".boundingelem",{
+        y: '0',        
+        duration:2,
+        ease:Expo.easeInOut
+        
+    })
+
+    
+}
+
+
+function circleMouseFollower(){
+    window.addEventListener("mousemove", function(dets){
+     document.querySelector("#smallcircle").style.transform = `translate(${dets.clientX}px,${dets.clientY}px)`;
+
+    })
+}
+
+circleMouseFollower();
+firstPageAnim();
