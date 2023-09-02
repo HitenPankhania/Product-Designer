b15f011b73db5b63a9aa81c2e9f3917c1814b5c0
Hiten Pankhania <pankhaniahiten4@gmail.com>
2023-09-02 18:09:22 +0530

    okiee

--- script.js
+++ script.js
@@ -31,13 +31,58 @@ function firstPageAnim(){
     
 }
 
+// function circleChaptaKaro() {
+//     //define default scale value
+//     var xscale = 1;
+//     var yscale = 1;
 
-function circleMouseFollower(){
+//     var xprev = 0;
+//     var xprev = 0;
+
+//     window.addEventListener("mousemove", function(dets){
+//         xscale = gsap.utils.clamp(.8, 1.2, dets.clientx -xprrev);
+//         xscale = gsap.utils.clamp(.8,1.2, dets.clientY - yprev);
+
+//         xprev = dets.clientx;
+//         yprev = dets.clientY;
+
+//         circleMouseFollower(xscale,yscale);
+//     } )
+// }
+
+
+
+
+
+
+function circleMouseFollower(xscale,yscale){
     window.addEventListener("mousemove", function(dets){
-     document.querySelector("#smallcircle").style.transform = `translate(${dets.clientX}px,${dets.clientY}px)`;
+     document.querySelector("#minicircle").style.transform = `translate(${dets.clientX}px,${dets.clientY}px)`;
 
     })
 }
-
+// circleChaptaKaro();
 circleMouseFollower();
 firstPageAnim();
+
+
+
+document.querySelectorAll(".elem").forEach(function(elem){
+    var rotate = 0;
+    var diffrot = 0;
+
+    
+
+    elem.addEventListener("mousemove",function(details){
+        var diff = dets.clientY - elem.getBoundingClientRect().top;
+        diffrot = dets.clientX - rotate;
+        rotate = dets.clientX;
+        gsap.to(elem.querySelector("img"),{
+            opacity:1,
+            ease:Power3,
+           top: diff,
+           left:dets.clientX,
+           rotate: gsap.utils.clamp(-20,20, diffrot *0.2)
+        })
+    })
+})
